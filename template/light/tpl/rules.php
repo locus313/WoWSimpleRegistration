d2d26974b313bb92d5409c81c9b483a4f5f2925e
locus313 <pat@lo5t.com>
2020-07-25 22:04:55 -0700

    Updated rules.php to initial rules.

--- template/light/tpl/rules.php
+++ template/light/tpl/rules.php
@@ -15,3 +15,18 @@
     <li>Rule 1.</li>
 </ul>
 -->
+
+<p>
+Rules:
+</p<
+<ul>
+    <li>Please keep to the spirit of the retail game.<br> No Botting. hacking, explioting, or acting in any manner to give an unfair edge to a player. This includes custom features that may not have been intended, and should be reported when noticed.
+No more than 2 accounts per person. Multi boxing is not allowed.</li><br>
+    <p>The same rules for discord chat apply to in game chat as well:</p>
+    <li>Be respectful of your fellow players and staff. Impersonation of staff is a serious offense.</li>
+    <li>Do not spread false or speculative information relating to realms, or staff, and do not share information that has not been stated in official announcements from staff members. Check the discord for official announcements.</li>
+    <li>Advertising of any products or services is not permitted. This includes advertising of other projects and your personal businesses, as well as selling characters and other services
+in game. It will result in a ban.</il>
+    <li>Racism - Sexism - Violence - Sexual Orientation Slurs - Hate Speech of ANY kind - Drugs and any illegal or illicit activity - Excessive derogatory or explicit language - Pornographic or otherwise inappropriate links. If you encounter this behavior on any of our platforms, please notify a staff member immediately via Discord.<br> Constructive criticism, issue reporting, and inquiry is welcome. This helps us improve the playing experience for everyone, just make sure you are engaging in a civil manner. Criticism with only intent to "bash" or "troll" will
+not be tolerated.</li>
+</ul>
