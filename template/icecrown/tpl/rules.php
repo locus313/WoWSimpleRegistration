0cd024f0da405333788df1227e6456e96667a86d
locus313 <pat@lo5t.com>
2020-07-25 22:59:12 -0700

    Update rules.php

--- template/icecrown/tpl/rules.php
+++ template/icecrown/tpl/rules.php
@@ -11,11 +11,14 @@
     For registration, you must accept following rules:
 </p>
 <ul>
-    <li>Rule 1.</li>
-    <li>Rule 2.</li>
-    <li>Rule 3.</li>
-    <li>Rule 4.</li>
-    <li>Rule 5.</li>
-    <li>Rule 6.</li>
-    <li>Edit rules on <b>"/template/<?php echo $antiXss->xss_clean(get_config("template")); ?>/tpl/rules.php"</b>.</li>
+    <li>Please keep to the spirit of the retail game.<br> No Botting. hacking, explioting, or acting in any manner to give an unfair edge to a player. This includes custom features that may not have been intended, and should be reported when noticed.
+No more than 2 accounts per person. Multi boxing is not allowed.</li>
+    <li>Be respectful of your fellow players and staff. Impersonation of staff is a serious offense.</li>
+    <li>Do not spread false or speculative information relating to realms, or staff, and do not share information that has not been stated in official announcements from staff members. Check the discord for official announcements.</li>
+    <li>Advertising of any products or services is not permitted. This includes advertising of other projects and your personal businesses, as well as selling characters and other services
+in game. It will result in a ban.</il>
+    <li>Racism - Sexism - Violence - Sexual Orientation Slurs - Hate Speech of ANY kind - Drugs and any illegal or illicit activity - Excessive derogatory or explicit language - Pornographic or otherwise inappropriate links. If you encounter this behavior on any of our platforms, please notify a staff member immediately via Discord.</li>
+<li>Constructive criticism, issue reporting, and inquiry is welcome. This helps us improve the playing experience for everyone, just make sure you are engaging in a civil manner. Criticism
+with only intent to "bash" or "troll" will
+not be tolerated.</li>
 </ul>
